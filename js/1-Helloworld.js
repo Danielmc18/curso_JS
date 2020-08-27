02cfd795156f28e585cfb074cc81f279987d0d9a
Daniel <daniel.montemayorcarlin@hotmail.com>
2020-08-26 20:33:12 -0500

    formas de mostrar texto en pantalla

---
+++ js/1-Helloworld.js
@@ -0,0 +1,16 @@
+ console.log("Hello World")
+
+ document.write("Hola Mundo")
+
+ alert("Hola mundo")
+
+ prompt("Hola Mundo")
+
+ confirm("Hola mundo")
+
+// Estas son unas maneras de mostar texto con JS 
+// *en la consola Primero,luego en el documento en si, 
+// el de "alert" es cuando abras/recarges la pagina te saltara
+// ese mensaje el de "prompt" es un mensaj que te muestra la 
+// pagina pero puedes "responder" algo y finalmente el de 
+// "confirm" esun mensaje que te salta y solo lo puedes ver

c46769bf86842f25fae049379180b4b38c795098
Daniel <daniel.montemayorcarlin@hotmail.com>
2020-08-26 20:44:47 -0500

    En este archivo estan todos los tipos de datos que hay

---
+++ js/3-tiposdedatos.js
@@ -0,0 +1,30 @@
+/*
+    Tipos de Datos:
+    numeros
+    strings
+    variablesyconstantes
+    booleans
+*/
+
+// Variable string es una cadena de caracteres "" 
+
+//Valores booleans es para el true y el false, el typeof es para saber
+// de que tipo de dato es
+
+/* 
+    Valores de comparacion:
+    nombre1===nombre2 (que si son iguales//los mismos)[usar 3 iguales para mejor trabajo]
+    nombre1!=nombre2 (que si nombre 1 es diferente o igual que numero2 )
+
+    numero1>numero2 (que si numero 1 es mayor que numero 2)
+    numero1<numero2 (que si numero 1 es menor que numero 2)
+    
+    numero1>=numero2 (que si numero 1 es mayor ó igual a numero 2)
+    numero2<=numero2 (que si numero 2 es menor ó igual a numero 2)
+*/
+
+let numero1 = "4"
+let numero2 = "7"
+
+
+

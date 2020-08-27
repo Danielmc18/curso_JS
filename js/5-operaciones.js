91e622e593723178639389c3cd2b8fe5340dffe1
Daniel <daniel.montemayorcarlin@hotmail.com>
2020-08-26 20:47:33 -0500

    Diferentes tipos de operaciones

---
+++ js/5-operaciones.js
@@ -0,0 +1,16 @@
+console.log(5+5) ///suma
+console.log(5-5) ///resta
+console.log(5/5) ///division
+console.log(5%5) ///resto de la division
+console.log(5*5) //multiplicacion
+
+
+///Para hacer una concataciones es como abajo y se agrega el +
+let texto1 = "Hola"
+let texto2 = "Mundo"
+let numero = 5
+let valor = true
+let numeromasdos = numero+2
+console.log("Mi variable texto1 contiene a "+texto1 + " Y mi texto2 "+texto2 )
+console.log("Mi numero es "+numero + " Mi valor es " +valor + " Mi numero mas dos es "
++ numeromasdos)

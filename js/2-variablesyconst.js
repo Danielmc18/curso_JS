47fa132783d0eb211badde45dc654b696c030a06
Daniel <daniel.montemayorcarlin@hotmail.com>
2020-08-26 20:43:13 -0500

    En este archivo es para mostrar las variables y constantes

---
+++ js/2-variablesyconst.js
@@ -0,0 +1,19 @@
+//variable es una forma de almacenar algo se define como "let"
+//para el texto es con "" y puede "variar"
+let saludar = "Hola"
+console.log(saludar)
+
+saludar = "que haces"
+alert(saludar)
+
+saludar = "Nada"
+prompt(saludar)
+
+saludar = "Y tu?"
+confirm(saludar)
+
+
+//// Ahora una constante es una variable fija por lo tanto ya no puede cambiar
+
+const saludo2 = "Hola a Todos soy una constante lol"
+console.log(saludo2)

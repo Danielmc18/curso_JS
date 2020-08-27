9ee722bbf8024bf4f55ab4f40fbc9c1d38ebac99
Daniel <daniel.montemayorcarlin@hotmail.com>
2020-08-26 20:46:07 -0500

    Archivo donde estan los condicionales en js

---
+++ js/4-condicionales.js
@@ -0,0 +1,49 @@
+/* Un condicional es algo que tiene que pasar pero se tiene que complir algo "Sí"
+tmb cuando un condicional se cumple*/
+
+//en el parentesis va la condicion y dentro de los corchetes va lo que tiene que 
+//hacer el if(ejecutar)
+
+let numero1 = 20
+let numero2 = 7
+
+// if(numero2>numero1)
+// {
+//     console.log("Es mayor")
+//     if(numero2===6)
+//     {
+//         console.log("Es 6")
+//     }
+// }
+// else if(numero1===14)
+// {
+//     console.log("Es 14")
+// }
+// else
+// {
+//     console.log("Ni numero 2 es mayor que numero1, ni numero1 es 14")
+// }
+
+
+///.....................................................................
+/// switch  ................................................................
+
+// switch (numero1){
+//     case 16:
+//         console.log("Es 16")
+//         break;
+//     case 18:
+//         console.log("Es 18")
+//         break;
+//     default:
+//         console.log("Ni es 16 ni es 18")
+//         break;
+// }
+
+
+//..........................................................................
+///Operador Ternario........................................................
+// En este operador se usa el "?" para evaluarlo despues se pone la condicion 
+// verdader y despues si llega a ser falsa entre las dos van los ":"
+
+numero1>numero2 ? console.log("Verdadero") : console.log("Falso")
